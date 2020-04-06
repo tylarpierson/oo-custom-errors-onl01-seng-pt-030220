d7cef7df23bf290d4ca9647d55a2b2e108888ed7
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 18:56:59 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -17,7 +17,9 @@ class Person
 end
 
 class PartnerError < StandardError 
-  
+  def message 
+    "you must give the get_married method an argument of an instance of the person class!"
+  end 
 end 
 
 beyonce = Person.new("Beyonce")
